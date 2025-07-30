13b2374d09ef473cf26ed7709a902e259969b77f
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2025-07-30 10:08:05 -0300

    adicionando style

--- components/title.tsx
+++ components/title.tsx
@@ -12,7 +12,7 @@ export function Title({ text, description }: TitleType) {
 
         <div className="flex flex-col gap-2 bg-red">
 
-            <h1 className="text-2xl">{text} - esse foi o titulo recebido como prop</h1>
+            <h1 className="text-2xl font-bold">{text} - esse foi o titulo recebido como prop</h1>
 
             <p className="font-light">{description}</p>
 
